b3b4b08f3517e1fe4a285564df920174a97bafd2
Hugo Carreira <hugo.colombo23@gmail.com>
2017-05-10 14:59:32 -0300

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res, next) {
-  res.render('index', { 'title': 'test', 'subtitle': 'test' });
+  res.render('index', { title: 'test', subtitle: 'test' });
+});
+
+/* GET user page */
+router.get('/user', function(req, res, next) {
+  res.render('user', { title: 'test u'});
 });
 
 module.exports = router;
